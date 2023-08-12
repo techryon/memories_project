4204eb918b7535559a37d7548918df26dd435b86
John E <techryon@gmail.com>
2023-08-12 20:36:40 +1200

    vite revert back

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,12 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    // generate manifest.json in outDir
-    manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: "../../server/index.js",
-    },
-  },
+  plugins: [react()],
 });
